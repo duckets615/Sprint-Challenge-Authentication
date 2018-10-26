254c00cb0a93ae8fe50054d75ab5e34a8a21e56d
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-10-26 15:06:00 -0400

    route probs, switch to asynch

--- config/routes.js
+++ config/routes.js
@@ -1,71 +1,73 @@
-const axios = require('axios');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const { authenticate } = require('./middlewares');
-
-const db = require('../database/dbConfig.js');
-
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
+const { authenticate, tokenGeneration } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
+async function register(req, res) {
+  try {
+    const credentials = req.body;
 
-function register(req, res) {
-  const creds = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 10);
 
-  const hash = bcrypt.hashSync(creds.password, 10);
-  creds.password = hash;
+    credentials.password = hash;
 
-  db('users')
-    .insert(creds)
-    .then(ids => {
-      const id = ids[0];
-      res
-        .status(201)
-        .json({ newUserId: id });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json(err);
-    });
-};
+    const newUserId = await db("users").insert(credentials);
+    try {
+      const user = await db("users")
+        .where({ id: newUserId[0] })
+        .first();
 
-//  JSON token
-const jwtSecret = 'that/s.my.secret.Cap,I/m.always.angry';
-
-function generateToken(user) {
-  const jwtPayload = {
-    ...user,
-    roles: ['admin', 'root'],
-  };
-  const jwtOptions = {
-    expiresIn: '1m',
-  };
-
-  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+      const token = tokenGeneration(user);
+      req.headers.authorization = token;
+      return res.status(200).send(token);
+    } catch (error) {
+      return res.status(404).json({ message: "the user does not exist" });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "the user could not be registered" });
+  }
 }
 
+async function login(req, res) {
+  try {
+    const credentials = req.body;
+    const user = await db("users")
+      .where({ username: credentials.username })
+      .first();
 
+    if (user && bcrypt.compareSync(credentials.password, user.password)) {
+      const token = tokenGeneration(user);
 
-
-function login(req, res) {
-  // implement user login
+      req.headers.authorization = token;
+      return res.status(200).send(token);
+    } else {
+      return res.status(404).json({ message: "Please register." });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "an error occurred during the login procedure",
+      error: error.message
+    });
+  }
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
