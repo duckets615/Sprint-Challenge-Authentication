7172294b622cc55399e0b213d34094cd703099e0
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-05 13:33:59 -0400

    generate token

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,2 +1,8 @@
 const jwt = require("jsonwebtoken");
 const jwtKey = require("../_secrets/keys").jwtKey;
+
+function tokenGeneration(user) {
+  const payload = { username: user.username };
+  const options = { expiresIn: 1000 * 60 * 60 };
+  return jwt.sign(payload, jwtKey, options);
+}
