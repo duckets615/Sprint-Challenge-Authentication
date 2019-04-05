520d2439f869f209d371e8a6eefcc9129d07e97f
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-05 14:00:54 -0400

    login

--- config/routes.js
+++ config/routes.js
@@ -31,3 +31,22 @@ function register(request, response) {
     response.status(500).json(error);
   })
 }
+
+function login(request, response) {
+  const credentials = request.body;
+
+  db('users')
+  .where({ username: credentials.username })
+  .then(user => {
+    user = user[0]
+    if (user) {
+      const token = jwt.sign({ username: credentials.username }, jwtKey, { expiresIn: '10m' });
+      response.status(200).json({ welcome: user, token })
+    } else {
+      response.status(401).json({ notAuthorized: "Unable to find a user with the provided credentials." })
+    }
+  })
+  .catch ( error => {
+    response.status(500).json(error)
+  })
+}
